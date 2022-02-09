04d8f2fc8a3cf8e513af6774fe429ace9bee9ad2
unknown <bruno_rios@estudante.sesisenai.org.br>
2022-02-08 23:20:08 -0300

    novos arquivos

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let atividades =[]
+
+
+function addAtividade() {
+    let dataAtividade = document.getElementById("data")
+    let atividade = document.getElementById("atividade")
+    if (localStorage.getItem("atividades") != null)
+    atividades = JSON.parse(localStorage.getItem("atividades"))
+    atividades.push([dataAtividade.value, atividade.value])
+    localStorage.setItem("atividades", JSON.stringify(atividades))
+    dataAtividade.value = ""
+    atividade.value = ""
+}
+function lista(){
+let pAtividades = document.getElementById('atividades')
+pAtividades.innerHTML = ""
+if (localStorage.getItem("atividades") != null)
+atividades = JSON.parse (localStorage.getItem("atividades"))
+atividades.forEach(atividade =>{
+    pAtividades.innerHTML += atividade[0] + ":" + atividade[1] + "<br>"
+});}
